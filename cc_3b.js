059e1c80de79d9881fc41a03637d14b1c85dccfc
Ohm Patel <ohmp3@usf.edu>
2026-02-08 08:37:43 -0500

    Modelled the Inventory

---
+++ cc_3b.js
@@ -0,0 +1,29 @@
+// Step 2: Model the Inventory
+
+let inventory = [
+    {
+        sku: "SKU-001",
+        name: "Chocolate Bronwie",
+        price: 2.99,
+        stock: 75
+    },
+    {
+        sku: "SKU-002",
+        name: "Double Chocolat Chunk Bronwie",
+        price: 3.99,
+        stock: 50
+    },
+    {
+        sku: "SKU-003",
+        name: "Mint Chocolate Bronwie",
+        price: 2.99,
+        stock: 65
+    },
+    {
+        sku: "SKU-004",
+        name: "Chocolate Bronwie",
+        price: 3.50,
+        stock: 95
+    }
+];
+
